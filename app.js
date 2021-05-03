c65bcb439915e98d2411aa037c2e0fbb5d37cb88
hamtaro702 <77424586+hamtaro702@users.noreply.github.com>
2021-05-03 18:01:17 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! My Name is Ham!!!!!!!!!!!'));
+app.get('/', (req, res) => res.send('Hello World! DEVOpS!!!!!!!@@@@@@@@@'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
