c690c41006de482b7db1f36a61fadccb5b1c7198
hamtaro702 <77424586+hamtaro702@users.noreply.github.com>
2021-05-09 14:05:51 +0700

    add slack fail noti

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const sum = require('./sum');
+
 
 app.get('/', (req, res) => res.send('Hello World! DEVOpS!!!!!!123!@@@@@@@@@'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+console.log(sum(1,2));
