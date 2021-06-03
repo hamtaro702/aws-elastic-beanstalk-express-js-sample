52898be79802dbac8cbc2cec0fad3ea38f1cf672
hamtaro702 <77424586+hamtaro702@users.noreply.github.com>
2021-06-03 23:38:10 +0700

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const sum = require('./sum');
+
 
 
 app.get('/', (req, res) => res.send('Hello World! DEVOpS!!!!!!123!@@@@@@@@@'));
@@ -9,4 +9,3 @@ app.get('/', (req, res) => res.send('Hello World! DEVOpS!!!!!!123!@@@@@@@@@'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
 
-console.log(sum(1,2));
